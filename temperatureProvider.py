aad8821adf4a8a0df0bcaedc30b94f74e2dc69f2
unknown <yael346@gmail.com>
2019-12-29 22:28:43 +0200

    fixes

--- temperatureProvider.py
+++ temperatureProvider.py
@@ -2,12 +2,17 @@ import json
 import sys
 from requests import get
 
+access_key = "c0881b3c46a0a56b1812c95f9dfcafbd"
+
 cities = sys.argv[1]
-citylist = cities.split(",")
+cities = cities.replace(",", ";")
+citylist = cities.split(";")
+units = "m"
 degrees = "celsius"
 if len(sys.argv) > 2:
     degrees = sys.argv[2]
     if degrees == "-f":
+        units = "f"
         degrees = "fahrenheit"
     elif degrees == "-c":
         degrees = "celsius"
@@ -15,16 +20,19 @@ if len(sys.argv) > 2:
         print("Invalid degrees term :(")
         exit(-1)
 
+params = {
+    'access_key': access_key,
+    'units': units
+}
+
 for city in citylist:
     city = city.capitalize()
-    weatherData = get("http://api.weatherstack.com/current?access_key=c0881b3c46a0a56b1812c95f9dfcafbd&query=" + city)
+    params["query"] = city
+    weatherData = get("http://api.weatherstack.com/current", params)
     weatherJson = json.loads(weatherData.text)
     if ('success' in weatherJson) and (weatherJson["success"] is False):
         print("The city doesn't exist. You should try other cities such as: Dublin, New-York, Ashdod :)")
     else:
         temperature = weatherJson["current"]["temperature"]
-        if degrees == "fahrenheit":
-            temperature = temperature * 9 / 5 + 32
-
         print("The weather in " + city + " today " + str(temperature) + " " + degrees + ".")
 
