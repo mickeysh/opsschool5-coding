d689e51fc756e5e42dac3d98947010804f6fdb3c
Yael <yael346@gmail.com>
2020-03-14 18:28:47 +0200

    add click cli to boto3

--- aws-cli.py
+++ aws-cli.py
@@ -1,16 +1,36 @@
+import click
 import boto3
 from tabulate import tabulate
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()['Reservations']
-table = [['Id', 'State', 'DNS']]
-for i in range (len(response)):
-    instance = response[i]['Instances'][0]
-    Id = instance['InstanceId']
-    State = instance['State']['Name']
-    try:
-        DNS = instance['PublicDnsName']
-    except:
-        DNS = None
-    table.append([Id, State, DNS])
-print(tabulate(table))
+@click.group()
+@click.option('-t', '--tags')
+@click.option('-c', '--cloud', type=click.Choice(['aws', 'gcp', 'azure']))
+def cli(tags, cloud):
+    """ Cloud Manager cli """
+
+@cli.group()
+def get():
+    """ get action """
+
+@get.command()
+def instances():
+    ec2 = boto3.client('ec2')
+    response = ec2.describe_instances()['Reservations']
+    table = [['Id', 'State', 'DNS']]
+    for i in range(len(response)):
+        instance = response[i]['Instances'][0]
+        Id = instance['InstanceId']
+        State = instance['State']['Name']
+        try:
+            DNS = instance['PublicDnsName']
+        except:
+            DNS = None
+        table.append([Id, State, DNS])
+    print(tabulate(table))
+
+@cli.command()
+def stop():
+    """ stop action """
+    click.echo('Stop')
+if __name__ == '__main__':
+    cli()
